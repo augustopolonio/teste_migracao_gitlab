cc9d38c6f8aa0b78377b680955ca880428aac237
Augusto Polonio <augustodrp@gmail.com>
2022-03-21 16:15:25 -0300

    2983: Primeiro commit (teste)

---
+++ Tutorial/Services/Login.svc.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Tutorial.Services
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Login" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Login.svc or Login.svc.cs at the Solution Explorer and start debugging.
+    public class Login : ILogin
+    {
+        public void DoWork()
+        {
+        }
+
+        public static void CreateUser()
+        {
+            TUTORIALEntities context = new TUTORIALEntities();
+
+            USUARIOS user = new USUARIOS();
+            user.NOME = "Augusto Polonio 2";
+            user.SENHA = "202CB962AC59075B964B07152D234B70";
+            user.EMAIL = "augustodrp@gmail.com";
+            user.DATA_NASCIMENTO = DateTime.Now;
+            context.USUARIOS.Add(user);
+            context.SaveChanges();
+        }
+
+        public static void ReadUser()
+        {
+            TUTORIALEntities context = new TUTORIALEntities();
+
+            USUARIOS user = context.USUARIOS.Single(c => c.ID == 1);
+
+            USUARIOS[] users = context.USUARIOS.ToArray();
+        }
+
+        public static void UpdateUser(int id)
+        {
+            TUTORIALEntities context = new TUTORIALEntities();
+            USUARIOS user = context.USUARIOS.Single(c => c.ID == id);
+
+            user.NOME = "Augusto Polonio 1 (Editado)";
+            context.SaveChanges();
+        }
+
+        public static void DeleteUser(int id)
+        {
+            TUTORIALEntities context = new TUTORIALEntities();
+            USUARIOS user = context.USUARIOS.Single(c => c.ID == id);
+
+            user.DATA_EXCLUSAO = DateTime.Now;
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Tutorial/index.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Tutorial.Services;
+
+namespace Tutorial
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Services.Login.CreateUser();
+            //Services.Login.ReadUser();
+            //Services.Login.UpdateUser(1);
+            Services.Login.DeleteUser(1);
+        }
+    }
+}
